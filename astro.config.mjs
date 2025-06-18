f7465e3b032bac9c465d7510a35366ef3c4f4e17
afcocac <afcocac@gmail.com>
2025-06-18 22:44:15 +0100

    definir el directorio base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://innovapaz.github.io",
-  // base: "",
+  base: "./",
   prefetch: true,
   trailingSlash: "never",
   experimental: {
